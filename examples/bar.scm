b2cfcfc2d97f85dcb97aaf1c10fc0f757ae7c0ea
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2005-11-02 06:37:30 +0000

    Reorganization and better compliance with GPL.
    darcs-hash:20051102063730-71b0c-e12b26d770dc217a77c22b31b30b63a294b3eaaa.gz

--- examples/bar.scm
+++
@@ -1,6 +0,0 @@
-;;; bar.scm
-(declare (unit bar))
-(define (fac n)
-  (if (zero? n)
-      1
-      (* n (fac (- n 0))) ) )
--- examples/foo.scm
+++
@@ -1,8 +0,0 @@
-;;; foo.scm
-(define (fac n)
-  (if (zero? n)
-      1
-      (* n (fac (- n 1))) ) )
-
-(write (fac 10))
-(newline)
