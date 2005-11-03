682ee8be84d51e19f7fda5ba19c37a68b2966c64
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2005-11-03 15:59:44 +0000

    sc-ofl to have a procedure called sc-ofl that reports itsefl.
    darcs-hash:20051103155944-71b0c-bd5c2c985fcf3b478e810b01bcf364fdd81c51a1.gz

--- examples/sc-ofl/sc-ofl.scm
+++ examples/sc-ofl/sc-ofl.scm
@@ -6,8 +6,7 @@
 ;;;; scons-chicken is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 ;;;; You should have received a copy of the GNU General Public License along with scons-chicken; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-(declare (unit bar))
-(define (fac n)
-  (if (zero? n)
-      1
-      (* n (fac (- n 0))) ) )
+(declare (unit sc-ofl))
+
+(define (sc-ofl)
+  (display "Hello from sc-ofl, the scons-chicke one-file-library."))
