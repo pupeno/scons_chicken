88c584858383ac73e083ec92bd5d24cf2a2ac87a
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2005-11-05 17:41:41 +0000

    Make sc-ofp call sc-ofl and print something by itself.
    darcs-hash:20051105174141-71b0c-3d2d23aee594bbe3ca4d03ca0286b31cc2f77013.gz

--- examples/sc-ofp/sc-ofp.scm
+++ examples/sc-ofp/sc-ofp.scm
@@ -6,10 +6,13 @@
 ;;;; scons-chicken is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 ;;;; You should have received a copy of the GNU General Public License along with scons-chicken; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-(define (fac n)
-  (if (zero? n)
-      1
-      (* n (fac (- n 1))) ) )
+(declare (uses sc-ofl))
 
-(write (fac 10))
-(newline)
+;;; An example of a procedure defined on a program that uses a procedure from an extension.
+;;; It is named sc-ofp-proc to avoid conflict/confusion with the program itself.
+(define (sc-ofp-proc)
+  (display "Hello from sc-ofp, the scons-chicken one-file-program.")
+  (newline)
+  (sc-ofl-proc))
+
+(sc-ofp-proc)
