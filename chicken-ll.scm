d58577a0dd05e471fab976ef840f909902950d99
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2005-11-06 04:41:28 +0000

    Succesfully print the list of libraries that a file needs to link against. (the ultimate goal).
    darcs-hash:20051106044128-71b0c-74f73b78c7538d0e200942b118967c795b4a4b7e.gz

--- chicken-ll.scm
+++ chicken-ll.scm
@@ -10,7 +10,7 @@
 (define rest cdr)
 
 ;;; Returns a list of all the names of the extensions being used by the source code that is get from the port file-port (to be used with call-with-input-file).
-(define (extension-names file-port)
+(define (extract-requirements file-port)
   (let process-form ((form (read file-port))) ; Read a form from form-port
     (if (eof-object? form)                    ; If it is eof
 	'()                                   ; return the empty list.
@@ -22,25 +22,44 @@
 	    (process-form (read file-port)))))) ; Otherwise keep processing.
 
 ;;; Find requirements returns a list of all the extensions that are required by the files (sources) passed as arguments.
-(define (find-all-requirements file-names)
+(define (get-requirements file-names)
   (if (null? file-names) ; If the list of files name is null (empty)
       '()                ; return null.
-      (let ((file-name (car file-names)))                         ; Otherwise, pick the first file
-	(append (call-with-input-file  file-name extension-names) ; get the extensions
-		(find-all-requirements (rest file-names))))))     ; and process tho other files.
+      (let ((file-name (first file-names)))                  ; Otherwise, pick the first file
+	(lset-union eq?                                      ; make an union of
+		    (call-with-input-file  file-name         ; its requirements
+		      extract-requirements)
+		    (get-requirements (rest file-names)))))) ; and process tho other files.
 
-;;;Make a regexp that will match any string of a list of strings.
+;;; Make a regexp that will match any string of a list of strings.
 (define (regexp-match-any strs)
   (string-append "(" (string-join strs "|") ")"))
- 
+
+;;; Having a list of .setup files, build the list of the libraries in them (the .so files).
+(define (get-libraries file-names)
+  (if (null? file-names)
+      '()
+      (let ((file-name (first file-names)))
+	(lset-union eq?
+		    (call-with-input-file file-name
+		      (lambda (file-port)
+			(rest (first (read file-port)))))
+		    (get-libraries (rest file-names))))))
+
 ;; Get the list of files passed as arguments to this program.
 (define files (rest (argv)))
 
 ;; Get a list of the extensions needed by those files.
-(define extensions (map symbol->string (find-all-requirements files)))
+(define extensions (map symbol->string (get-requirements files)))
 
 ;; Build the regexp to match any .setup file of the extensions we need.
 (define ext-regexp (string-append (repository-path) "/" (regexp-match-any extensions) "\\.setup"))
 
-(display (find-files (repository-path) ext-regexp))
-(newline)
+;; Build the list of .setup files of the extensions we need.
+(define setup-file-names (find-files (repository-path) ext-regexp))
+
+;; Build the list of libraries we need.
+(define libraries (get-libraries setup-file-names))
+
+;; Print the list of libraries with no parenthesis and separated by spaces.
+(display (string-join libraries " "))
