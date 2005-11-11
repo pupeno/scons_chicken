d933e9858010fccc3a71bf60062c5df3c531768d
José Pablo Ezequiel Fernández <pupeno@pupeno.com>
2005-11-11 14:41:00 +0000

    Added sc-mfp, scons-chicke multiple-file-program example.
    darcs-hash:20051111144100-71b0c-c4e63e432e7f029125a8fb8e14b1e880cebe31c3.gz

---
+++ examples/sc-mfp/sc-mfp2.scm
@@ -0,0 +1,15 @@
+;;;; Copyright (C) 2005 José Pablo Ezequiel "Pupeno" Fernández Silva
+;;;;
+;;;; This file is part of scons-chicken.
+;;;;
+;;;; scons-chicken is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+;;;; scons-chicken is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+;;;; You should have received a copy of the GNU General Public License along with scons-chicken; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+(declare (unit sc-mfp2))
+(use sc-mfl)
+
+(define (sc-mfp2-proc)
+  (display "Hello from sc-mfp2.scm of the scons-chicken multiple-file-program.")
+  (newline)
+  (sc-mfl2-proc))
